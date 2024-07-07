c28fe2e9809289e02d13a3c1d8bce6cc7795b897
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-07 21:23:29 +0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
