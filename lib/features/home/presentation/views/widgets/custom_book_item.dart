ce589b93078c4585cea1bca5a008894e569b211f
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-08 22:20:14 +0300

    crate customBookItem

---
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomBookItem extends StatelessWidget {
+  const CustomBookItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String testImage =
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtiJKuQkGMitVsqqDyAppVphB6JCTlpEl8_tytAzIVrG-z5NgLGQcQb8uCeQDF7ueRI0w&usqp=CAU';
+    return SizedBox(
+      height: 180.h,
+      child: AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(testImage),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,6 +12,7 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
+          CustomBookItem(),
         ],
       ),
     );
