b00ad53b898af8629e7047319eb6772bd9ff1497
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-10 13:37:49 +0300

    best seller list view item part 1

--- lib/core/constants/styles.dart
+++ lib/core/constants/styles.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/core/constants/fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -8,9 +7,9 @@ class AppStyles {
     fontWeight: FontWeight.w600,
   );
   static TextStyle textStyle20 = TextStyle(
-      fontSize: 20.sp,
-      fontWeight: FontWeight.normal,
-      fontFamily: AppFonts.playFairDisplay);
+    fontSize: 20.sp,
+    fontWeight: FontWeight.normal,
+  );
   static TextStyle textStyle14 = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeight.normal,
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/core/constants/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_book_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -25,39 +25,6 @@ class HomeViewBody extends StatelessWidget {
             ),
           ),
           const BestSellerListViewItem()
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125.h,
-      child: Row(
-        children: [
-          // image
-          const BestSellerBookItem(),
-          SizedBox(
-            width: 30.w,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: 200.w,
-                child: Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: AppStyles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              )
-            ],
-          )
         ],
       ),
     );
