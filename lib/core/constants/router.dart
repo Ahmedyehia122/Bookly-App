9126018bbee799aae72f2e8fe81acfe685466b3e
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-14 16:18:23 +0300

    create animation transition with GoRouter

--- lib/core/constants/router.dart
+++ lib/core/constants/router.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/constants/durations.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -7,7 +9,22 @@ abstract class AppRouter {
   static GoRouter router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
-      GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: kHomeView,
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            child: const HomeView(),
+            transitionDuration: AppDurations.kTransitionDuration,
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(
+                opacity: CurveTween(curve: Curves.easeIn).animate(animation),
+                child: child,
+              );
+            },
+          );
+        },
+      ),
     ],
   );
 }
