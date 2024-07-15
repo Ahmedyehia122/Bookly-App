c9db53c866ed8f0b5d0d180b88cc02eec2de2274
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-15 16:19:40 +0300

    refactor code

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,5 @@
-import 'package:bookly_app/core/constants/fonts.dart';
-import 'package:bookly_app/core/constants/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/Books_details_view_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/button_action.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -19,48 +14,14 @@ class BookDetailsViewBody extends StatelessWidget {
           hasScrollBody: false,
           child: Padding(
             padding: EdgeInsets.only(left: 30.sp, right: 30.sp),
-            child: Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const BookDetailsViewAppBar(),
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 90.w, vertical: 8.h),
-                    child: const CustomBookImage(),
-                  ),
-                  SizedBox(height: 15.h),
-                  Text(
-                    'The Jungle Book',
-                    style: AppStyles.textStyle30,
-                  ),
-                  Text(
-                    'Rudyadr Kipling',
-                    style: AppStyles.textStyle20.copyWith(
-                      fontFamily: AppFonts.playFairDisplay,
-                      color: Colors.white.withOpacity(.7),
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  SizedBox(height: 10.h),
-                  const CustomBookRating(),
-                  SizedBox(height: 30.h),
-                  const ButtonAction(),
-                  Expanded(child: SizedBox(height: 40.h)),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'You can also like',
-                      style: AppStyles.textStyle14.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SimilarBooksListView(),
-                  SizedBox(height: 30.h),
-                ],
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const BookDetailsSection(),
+                Expanded(child: SizedBox(height: 40.h)),
+                const SimilarBooksSection(),
+                SizedBox(height: 30.h),
+              ],
             ),
           ),
         )
