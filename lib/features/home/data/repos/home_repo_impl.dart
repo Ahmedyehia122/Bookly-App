d84426a4e791845be15cf39a91b853c7ccc88320
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-19 16:34:56 +0300

    create api service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooksListview() {}
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBooksListView() {}
+}
