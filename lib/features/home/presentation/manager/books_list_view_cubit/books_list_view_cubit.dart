44ca8e662b1fc0a87d5b6fbd81fe22294e56d997
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-20 11:52:55 +0300

    create books list view method

--- lib/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart
+++ lib/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'books_list_view_state.dart';
 
 class BooksListViewCubit extends Cubit<BooksListViewState> {
-  BooksListViewCubit() : super(BooksListViewInitial());
+  BooksListViewCubit(this.homeRepo) : super(BooksListViewInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchBooksListView() async {
+    emit(BooksListViewLoading());
+    var result = await homeRepo.fetchBooksListView();
+    result.fold((failure) {
+      emit(BooksListViewFailure(errMessage: failure.errMessage));
+    }, (books) {
+      emit(BooksListViewSuccess(books: books));
+    });
+  }
 }
